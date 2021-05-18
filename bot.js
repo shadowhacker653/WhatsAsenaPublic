56012d561d9f00879fbc27ab0efc7a39f18832c5
T0X!C D3V!L <jinnherefan2021@gmail.com>
2021-05-18 14:38:53 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -256,18 +256,17 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
         }
 
         if (msg.messageStubType === 32 || msg.messageStubType === 28) {
-            // Görüşürüz Mesajı
+            // see you message
             var gb = await getMessage(msg.key.remoteJid, 'goodbye');
             if (gb !== false) {
-
-                await conn.sendMessage(msg.key.remoteJid, gb.message, MessageType.text);
+                await conn.sendMessage(msg.key.remoteJid, fs.readFileSync("/root/WhatsAsenaPublic/media/gif/VID-20210518-WA0060.mp4"), MessageType.video, {mimetype: Mimetype.mpeg, caption: gb.message});
             }
             return;
         } else if (msg.messageStubType === 27 || msg.messageStubType === 31) {
-            // Hoşgeldin Mesajı
+            // Welcome message
             var gb = await getMessage(msg.key.remoteJid);
             if (gb !== false) {
-                await conn.sendMessage(msg.key.remoteJid, gb.message, MessageType.text);
+                await conn.sendMessage(msg.key.remoteJid, fs.readFileSync("/root/WhatsAsenaPublic/media/gif/VID-20210518-WA0059.mp4"), MessageType.video, {mimetype: Mimetype.mpeg, caption: gb.message});
             }
             return;
         }
