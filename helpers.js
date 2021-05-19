ad7dd294fe3cf6463c8ea1cc843818409c314f65
T0X!C D3V!L <jinnherefan2021@gmail.com>
2021-05-20 00:58:33 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *WhatsAsena*:  ```" + msg + "```"
+    return "✅ *WhatsAsenaPublic*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *WhatsAsena*:  ```" + msg + "```"
+    return "🛑 *WhatsAsenaPublic*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *WhatsAsena*:  ```" + msg + "```"
+    return "⏺️ *WhatsAsenaPublic*:  ```" + msg + "```"
 }
 
 
